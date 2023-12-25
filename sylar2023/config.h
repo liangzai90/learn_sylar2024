05ccdb5009c5178f5de5e1276b7d76d0836474d2
Henry <1073072541@qq.com>
2023-12-25 18:03:09 +0800

    chap15-配置变更事件。回调函数

--- sylar2023/config.h
+++ sylar2023/config.h
@@ -14,7 +14,7 @@
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
-
+#include <functional>
 
 namespace sylar{
 
@@ -239,6 +239,7 @@ template<class T , class FromStr = LexicalCast<std::string, T>
 class ConfigVar: public ConfigVarBase{
 public:
     typedef std::shared_ptr<ConfigVar> ptr;
+    typedef std::function<void (const T& old_value, const T& new_value) > on_change_cb;
 
     ConfigVar(const std::string& name, 
             const T& default_value,
@@ -271,10 +272,38 @@ public:
     }
 
     const T getValue() const {return m_val;}
-    void setValue(const T& v) {m_val = v;}
+    void setValue(const T& newV) {
+        if(newV == m_val){
+            return ;
+        }
+        for(auto& item : m_cbs){
+            item.second(m_val, newV);
+        }
+        m_val = newV;
+    }
     std::string getTypeName() const override { return typeid(T).name();}
+
+    void addListener(uint64_t key, on_change_cb cb){
+        m_cbs[key] = cb;
+    }
+
+    void delListener(uint64_t key){
+        m_cbs.erase(key);
+    }
+
+    on_change_cb getListener(uint64_t key){
+        auto it = m_cbs.find(key);
+        return it==m_cbs.end() ? nullptr : it->second;
+    }
+
+    void clearListener(){
+        m_cbs.clear();
+    }
+
 private:
     T m_val;
+    //变更回调函数组, uint64_t key要求唯一，一般可以用hash
+    std::map<uint64_t, on_change_cb> m_cbs;
 };
 
 class Config{
