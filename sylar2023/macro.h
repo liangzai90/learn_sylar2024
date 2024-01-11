7dc5bf211e881d0bef0ea91b00ebb04fc2d75e83
Henry <1073072541@qq.com>
2024-01-11 17:48:32 +0800

    chap39--finished--协程调度模块

--- sylar2023/macro.h
+++ sylar2023/macro.h
@@ -4,7 +4,19 @@
 #include <string.h>
 #include <assert.h>
 #include "util.h"
+#include "log.h"
 
+#if defined __GNUC__ || defined  __llvm__
+/// LIKCLY 宏的封装，告诉编译器优化
+# define   SYLAR_LIKELY(x)    __builtin_expect(!!(x), 1)
+/// LIKCLY 
+# define   SYLAR_UNLIKELY(x)   __builtin_expect(!!(x), 0)
+#else 
+# define SYLAR_LIKELY(x)   (x)
+# define SYLAR_UNLIKELY(x)    (x)
+#endif  
+
+///断言 宏 封装
 #define  SYLAR_ASSERT(x)  \
     if(!(x)) { \
         SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) <<"ASSERTION: "  #x \
